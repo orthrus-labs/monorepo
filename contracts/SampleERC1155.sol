b4c93240d486473e6c05aba7aae8fc6fa88661bf
Gabriele Rosati <g.rosati00@outlook.it>
2021-11-05 16:02:58 +0100

    add new functions in SampleERC1155.sol

--- contracts/SampleERC1155.sol
+++ contracts/SampleERC1155.sol
@@ -4,12 +4,30 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 
+contract SampleERC1155 is ERC1155Burnable, Ownable{
 
-contract SampleERC1155 is ERC1155Burnable{
+    address marketplaceAddress;
 
     constructor(string memory _uri)
         ERC1155(_uri)
     {}
+
+    function setMarketplaceAddress(address _marketplaceAddress) public onlyOwner{
+        marketplaceAddress = _marketplaceAddress;
+
+    }
+
+    modifier onlyMarketplace{
+        require(msg.sender == marketplaceAddress, "Only the marketplace can call this function");
+        _;
+    }
+
+    function mint(address account, uint256 id, uint256 amount, bytes memory data) public onlyMarketplace
+    {
+        _mint(account, id, amount, data);
+    }
+
 }
