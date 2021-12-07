7fe8a858d7bc189b3548f2ddf843daf43ea6175f
Gabriele Rosati <g.rosati00@outlook.it>
2021-12-07 15:20:14 +0100

    add externals and internals folders

---
+++ contracts/internals/Utils.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: Unlicense
+
+pragma solidity ^0.8.0;
+
+contract Utils {
+
+    // calculates a^(1/n) to dp decimal places
+    // maxIts bounds the number of iterations performed
+    function nthRoot(uint _a, uint _n, uint _dp, uint _maxIts) pure public returns(uint) {
+        assert (_n > 1);
+
+        // The scale factor is a crude way to turn everything into integer calcs.
+        // Actually do (a * (10 ^ ((dp + 1) * n))) ^ (1/n)
+        // We calculate to one extra dp and round at the end
+        uint one = 10 ** (1 + _dp);
+        uint a0 = one ** _n * _a;
+
+        // Initial guess: 1.0
+        uint xNew = one;
+        uint x = 1;
+
+        uint iter = 0;
+        while (xNew != x && iter < _maxIts) {
+            x = xNew;
+            uint t0 = x ** (_n - 1);
+            if (x * t0 > a0) {
+                xNew = x - (x - a0 / t0) / _n;
+            } else {
+                xNew = x + (a0 / t0 - x) / _n;
+            }
+            ++iter;
+        }
+
+        // Round to nearest in the last dp.
+        return (xNew + 5) / 10;
+    }
+
+    function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
+        if (_i == 0) {
+            return "0";
+        }
+        uint j = _i;
+        uint len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len;
+        while (_i != 0) {
+            k = k-1;
+            uint8 temp = (48 + uint8(_i - _i / 10 * 10));
+            bytes1 b1 = bytes1(temp);
+            bstr[k] = b1;
+            _i /= 10;
+        }
+        return string(bstr);
+    }
+}
